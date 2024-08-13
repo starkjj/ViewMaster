12d182f46d535dc70119eea661e8df8b5114ea49
Jeremy Stark <razestudiosdev@gmail.com>
2024-08-13 10:22:44 -0400

    Work on grain shader

--- Scenes/Components/slide.gd
+++ Scenes/Components/slide.gd
@@ -4,7 +4,7 @@ extends Node2D
 @export var slide_image:Texture2D: set = _set_slide_texture
 
 func _set_slide_texture(texture:Texture2D):
-	$ClippingFrame/KitchenScene/SlideSprite.texture = texture
+	$ClippingFrame/Scene/SlideSprite.texture = texture
 	slide_image = texture
 
 func _ready() -> void:
