247f2c8f811ee39cba9e20510f991927e58d5fee
starkjj <razestudiosdev@gmail.com>
2024-10-11 15:07:10 -0400

    A bunch of update images mainly

---
+++ Scenes/Components/slide_test.gd
@@ -0,0 +1,11 @@
+@tool
+extends Node2D
+
+@export var slide_image:Texture2D: set = _set_slide_texture
+
+func _set_slide_texture(texture:Texture2D):
+	$Scene/SlideSprite.texture = texture
+	slide_image = texture
+
+func _ready() -> void:
+	pass
